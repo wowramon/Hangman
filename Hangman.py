bed740fd13296c21d0d680a5fdf7cfa15c8b6c5e
Ramon Hernandez <alejandro4801@gmail.com>
2021-10-09 11:38:37 -0300

    Funcion loadData y comentarios

--- Hangman.py
+++ Hangman.py
@@ -1,9 +1,27 @@
+import os
 
 
 
+def loadData(): # Cargamos las palabras del archivo Data
+    
+  with open('Data.txt','r', encoding='utf-8') as Dp:  # Abrimos el archivo Data en modo solo lectura y le agregamos el formato de decodificacion utf-8
+      Words = [i for i in Dp ] # Comprehension list para almacenar el texto en variable
+  return Words
+  
+def board(): # Indica el tablero de juego
+    Trigger = False  # Armamos variable trigger para salir del loop
+    while Trigger:
+        print('Welcom to the hangman game') # Header
+        
+        Trigger = True # Salirmos del Loop
+        
+            
+   
 
-def run():
-    pass
+    
+     
+def run(): # Funcion principal
+   loadData()
 
 
 if __name__=='__main__':
