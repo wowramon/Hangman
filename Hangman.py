e2ae48dc53c479ae4b2a1f4af7660287fae3d9ba
Ramon Hernandez <alejandro4801@gmail.com>
2021-10-12 16:43:16 -0300

    Version 1 completo

--- Hangman.py
+++ Hangman.py
@@ -3,7 +3,7 @@ import random
 
 def loadData(): # Cargamos las palabras del archivo Data
     
-  with open('Data.txt','r', encoding = "utf-8") as Dp:  # Abrimos el archivo Data en modo solo lectura y le agregamos el formato de decodificacion utf-8
+  with open('Data.txt','r') as Dp:  # Abrimos el archivo Data en modo solo lectura y le agregamos el formato de decodificacion utf-8
       Words = [i for i in Dp ] # Comprehension list para almacenar el texto en variable
   return Words
   
@@ -12,7 +12,6 @@ def getRamdonW(): # Funcion que devuelve una palabra aleatoria
   word = text[random.randrange(len(text))] # Guardamos el texto en la variable indexando dentro de la lista con un numero aleatorio
   return word # devolvemos el resultado
     
-
 def SplitList(word,hidden): #Funcion que nos ayuda a dividir la palabra en una lista letra por letra, Ademas nos ayuda a crear una lista que oculta dichas letras
   newlist = [char for char in word if char != '\n'] #Comprehension list que nos ayuda a separa la palabra en letras
   if hidden : #Condicional 
@@ -29,20 +28,34 @@ def Display(List): # Funcion que nos despliega los espacios
       
    return display
 
-def Validate(listH,listW):  #Funcion que nos ayuda a validar la cadena de strings
+def Validate(listH,listW,bool):  #Funcion que nos ayuda a validar la cadena de strings
+    
     countH = 0  #Counts 
     countW = 0
     
-    for i in range(len(listH)):
-      if listH[i] != '_':  #Si el char es diferente al de _ suma 1 al contador
-        countH = countH+1
+    if bool == False:
+       for i in range(len(listH)):
+         if listH[i] != '_':  #Si el char es diferente al de _ suma 1 al contador
+           countH = countH+1
+           
+       for i in range(len(listW)):
+        countW = countW+1  #contador por cada char
         
-    for i in range(len(listW)):
-      countW = countW+1  #contador por cada char
+       return countH == countW  #valida si la cantidad de char es correcta
+    else:
+      
+       for i in range(len(listH)):
+         if listH[i] != '_':  #Si el char es diferente al de _ suma 1 al contador
+           countH = countH+1
+           
+       return countH
+           
+   
       
       
-    return countH == countW  #valida si la cantidad de char es correcta
     
+
+   
 def board(): # Indica el tablero de juego
     Trigger = True # Armamos variable trigger para salir del loop
     
@@ -53,7 +66,8 @@ def board(): # Indica el tablero de juego
     List = SplitList(word,False)
     HiddenList = SplitList(word,True)
     print(Display(HiddenList))  #Mostramos en pantalla los espacio a completar
-    
+    Lose_count = 0
+    n = 0
     while Trigger:
         
         input_word =input('Introduce una letra:\n ') #Ingresamos una letra
@@ -64,17 +78,30 @@ def board(): # Indica el tablero de juego
             if(item.lower() == List[i]): #Validamos la letra y la pasamos a minusculas con la funcion lower
               HiddenList[i] = item.lower() #Si coninciden agregamos dicho item en el index correspondiente
         
+        if Lose_count == Validate(HiddenList,HiddenList,True): #Comprueba si hay cambios en la lista si no las hay significa que fallo el intento
+           n = n + 1
+           
+        else:
+          Lose_count = Validate(HiddenList,HiddenList,True) #Igualamos la variable
+           
+          
         
-        if Validate(Display(HiddenList),Display(List)):
+        
+        if Validate(Display(HiddenList),Display(List),False):
          clear()
          print(Display(HiddenList)) #Mostramos en pantalla los espacio a completar
+         print('You WIN')
          Trigger = False # Salimos del Loop
+        elif n >= 11: 
+           print(Display(List))
+           print('You Lose')
+           Trigger = False # Salimos del Loop
         else:
          clear()
          print(Display(HiddenList))
-         print('You WIN')
+         print(str(n) +' Intentos fallidos')
          
-   
+          
 def run(): # Funcion principal
   board()
 
