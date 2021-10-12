b2b6e3411f63121f6fa36cfcaef00c5ad3617057
Ramon Hernandez <alejandro4801@gmail.com>
2021-10-12 11:07:02 -0300

    Avanzamos con el body del programa

--- Hangman.py
+++ Hangman.py
@@ -4,7 +4,7 @@ import re
 
 def loadData(): # Cargamos las palabras del archivo Data
     
-  with open('Data.txt','r', encoding="utf-8") as Dp:  # Abrimos el archivo Data en modo solo lectura y le agregamos el formato de decodificacion utf-8
+  with open('Data.txt','r', encoding = "utf-8") as Dp:  # Abrimos el archivo Data en modo solo lectura y le agregamos el formato de decodificacion utf-8
       Words = [i for i in Dp ] # Comprehension list para almacenar el texto en variable
   return Words
   
@@ -17,40 +17,47 @@ def getRamdonW(): # Funcion que devuelve una palabra aleatoria
 def SplitList(word,hidden): #Funcion que nos ayuda a dividir la palabra en una lista letra por letra, Ademas nos ayuda a crear una lista que oculta dichas letras
   newlist = [char for char in word] #Comprehension list que nos ayuda a separa la palabra en letras
   if hidden : #Condicional 
-      hiddenList = ['_' for char in word] # añadimos el caracter especial a una nueva lista dependiendo de la cantidad de letras que tenga la palabra
+      hiddenList = ['_' for char in word if char != '\n' ] # añadimos el caracter especial a una nueva lista dependiendo de la cantidad de letras que tenga la palabra
       return hiddenList # return
   else:
     return newlist # return
     
 def Display(List): # Funcion que nos despliega los espacios 
-   display = ' ' 
+   display = ''  #Inicializamos variable
    for i in range(len(List)):
       char = List[i]
       display = display + char #Cadena de caracteres
       
-   print(display)
+   return display
 
 
     
 def board(): # Indica el tablero de juego
-    Trigger = False  # Armamos variable trigger para salir del loop
+    Trigger = True # Armamos variable trigger para salir del loop
+    
+    print('Welcom to the hangman game') # Header
+    word = getRamdonW()  
+    List = SplitList(word,False)
+    HiddenList = SplitList(word,True)
+    print(Display(HiddenList))  #Mostramos en pantalla los espacio a completar
+    
     while Trigger:
-        print('Welcom to the hangman game') # Header
-        word = getRamdonW()  # Body
-        List = SplitList(word,False)
-        HiddenList = SplitList(word,True)
         
-        input_word =input('Introduce una letra o palabra:\n ')
-        
+        print(Display(List))
+        input_word =input('Introduce una letra:\n ') #Ingresamos una letra
+     
         for item in input_word.split():
-          for i in List:
-            if(item == i):
-              HiddenList[i] = item
+          for i in range(len(List)):           # Body
+            if(item.lower() == List[i]): #Validamos la letra y la pasamos a minusculas con la funcion lower
+              HiddenList[i] = item.lower() #
+               
         
-        Display(HiddenList)
-        
-        os.system('pause')
-        Trigger = True # Salimos del Loop
+        if str(Display(HiddenList)) is str(Display(List)):
+         print(Display(HiddenList)) #Mostramos en pantalla los espacio a completar
+         Trigger = False # Salimos del Loop
+        else:
+         print(Display(HiddenList))
+         
         
             
    
@@ -58,9 +65,10 @@ def board(): # Indica el tablero de juego
     
      
 def run(): # Funcion principal
-    word = getRamdonW()  # Body
-    List = SplitList(word,False)
-    Display(List)
+    # word = getRamdonW()  # Body
+    # List = SplitList(word,False)
+    # Display(List)
+    board()
 
 if __name__=='__main__':
     run()
